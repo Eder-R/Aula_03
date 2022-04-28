9aec931131b507ccb55d8fb27f48f962f4f557d5
Eder-R <eder31250@gmail.com>
2022-04-27 21:38:35 -0300

    Criado as opcoes: 'Alterar' e 'Apagar' livro

--- db.js
+++ db.js
@@ -1,25 +1,63 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise')
 
-async function connect() {
-  if(global.connection && global.connection.state !== 'disconected')
-  {
-      return global.connection
-  }
-  const connection = await mysql.createConnection({
-    host      : 'localhost',
-    user      : 'root',
-    password  : '',
-    database  : 'livraria',
-    port      : 3306
-  });
-  global.connection = connection
-  return connection;
+
+async function conectarBD()
+{
+    if (global.connection && global.connection.state !== 'disconnected')
+    {
+        return global.connection
+    }
+
+    const connection = await mysql.createConnection(
+        {
+            host     : 'localhost',
+            port     : 3306,
+            user     : 'root',
+            password : '',
+            database : 'livraria'
+        }
+    );
+
+    global.connection = connection
+    return global.connection
 }
 
-async function listarLivros() {
-  const connection = await connect();
-  const [registros] = await connection.query('SELECT * FROM livros');
-  return registros;
+
+async function listarLivros()
+{
+    const conexao = await conectarBD()
+    const [registros] = await conexao.query('select * from livros;')
+    return registros
 }
 
-module.exports = { listarLivros };
+async function inserirLivro(livro)
+{
+    const conexao = await conectarBD()
+    const sql = "insert into livros (livtitulo, livano, gencodigo) values (?,?,?);"
+    return await conexao.query(sql, [livro.titulo, livro.ano, livro.genero])
+}
+
+
+async function apagarLivro(codigo)
+{
+    const conexao = await conectarBD()
+    const sql = "delete from livros where livcodigo=?;"
+    return await conexao.query(sql,[codigo])
+}
+
+
+async function recuperarLivro(codigo)
+{
+    const conexao = await conectarBD()
+    const sql = "select * from livros where livcodigo=?;"
+    const [livro] = await conexao.query(sql,[codigo])
+    return livro [0]
+}
+
+async function alterarLivro(livro){
+    const conexao = await conectarBD()
+    const sql = "UPDATE livros SET livtitulo=?, livano=?, gencodigo=? WHERE livcodigo=?;"
+    return await conexao.query(sql,[livro.titulo, livro.ano, livro.genero, livro.codigo])
+}
+
+module.exports = { listarLivros, inserirLivro, apagarLivro, recuperarLivro, alterarLivro }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/',async function(req, res) {
-
-  const registros = await global.db.listarLivros();
-
+router.get('/', async function(req, res) {
+  const registros = await global.db.listarLivros()
   res.render('index', { registros });
 });
+
+
+router.get('/novoLivro', function(req, res){
+  res.render('formLivro', { titulo:'Novo livro', acao:'/novoLivro', livro:{}})
+})
+
+router.post('/novoLivro', async function(req, res) {
+  const titulo = req.body.edtTitulo
+  const ano = parseInt(req.body.edtAno)
+  const genero = parseInt(req.body.selGenero)
+
+  await global.db.inserirLivro({titulo,ano,genero})
+  res.redirect('/')
+} )
+
+router.get('/alteraLivro/:id', async function(req, res) {
+  const codigo = parseInt(req.params.id)
+  const livro = await global.db.recuperarLivro(codigo)
+  res.render('formLivro',{ titulo:'Alteração de livro', acao:'/alteraLivro/'+codigo,livro})
+})
+
+router.post('/alteraLivro/:id', async function(req, res) {
+  const codigo = parseInt(req.params.id)
+  const titulo = req.body.edtTitulo
+  const ano = parseInt(req.body.edtAno)
+  const genero = parseInt(req.body.selGenero)
+  await global.db.alterarLivro({codigo, titulo, ano, genero})
+  res.redirect('/')
+})
+
+router.get('/apagaLivro/:id', async function(req, res){
+  const codigo = parseInt(req.params.id)
+  await global.db.apagarLivro(codigo)
+  res.redirect('/')
+})
 
 
 
